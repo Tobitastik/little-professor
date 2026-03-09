9b2d7ad7a218cd8534031bb41d0c67f6a99c216e
Tobias <144216903+Tobitastik@users.noreply.github.com>
2026-03-09 18:11:34 +0100

    Change to drop down possition

--- src/App.jsx
+++ src/App.jsx
@@ -88,18 +88,17 @@ export default function App() {
       <audio ref={shortEndSound} src="/little-professor/sounds/shortfanfare.wav" preload="auto" />
       <audio ref={longEndSound} src="/little-professor/sounds/fanfare.wav" preload="auto" />
 
-      <select
-        value={difficulty}
-        onChange={(e) => setDifficulty(e.target.value)}
-        className="keypad-button"
-      >
-        <option value="easy">Easy (+)</option>
-        <option value="medium">Medium (+, -)</option>
-        <option value="hard">Hard (+, -, *)</option>
-        <option value="extreme">Extreme (+, -, *, /)</option>
-      </select>
-
       <div className="game-box">
+        <select
+          value={difficulty}
+          onChange={(e) => setDifficulty(e.target.value)}
+          className="keypad-button"
+        >
+          <option value="easy">Easy (+)</option>
+          <option value="medium">Medium (+, -)</option>
+          <option value="hard">Hard (+, -, *)</option>
+          <option value="extreme">Extreme (+, -, *, /)</option>
+        </select>
         <h2 className="title">Little Professor</h2>
 
 
